a78dfa304214ecb61d7fa5b21521f6b08e59e2cc
Julian Raab <raab.julian93@gmail.com>
2018-01-27 16:45:47 +0100

    pimcore live templates and view template

---
+++ Pimcore/Live Templates/Pimcore View.html.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by ${PRODUCT_NAME}.
+ * User: ${USER}
+ * Date: ${DATE}
+ * Time: ${TIME}
+ *
+ * @var \Pimcore\Templating\PhpEngine \$this
+ * @var \Pimcore\Templating\PhpEngine \$view
+ * @var \Pimcore\Templating\GlobalVariables \$app
+ */
